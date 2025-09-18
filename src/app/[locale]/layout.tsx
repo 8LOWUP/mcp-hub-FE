2e093723071cf06b039d38e6db61b95d3d27684f
heyn2 <hy702@gachon.ac.kr>
2025-09-18 16:17:00 +0900

    :sparkles: feat: 전체 페이지 공통 래퍼 375px 지정

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -4,28 +4,32 @@ import { getMessages } from "next-intl/server";
 import { ThemeProvider } from "@/contexts/theme-provider";
 
 export default async function RootLayout({
-                                           children,
-                                           params,
+                                             children,
+                                             params,
                                          }: {
-  children: React.ReactNode;
-  params: Promise<{ locale: string }>;
+    children: React.ReactNode;
+    params: Promise<{ locale: string }>;
 }) {
-  const { locale } = await params;
+    const { locale } = await params;
 
-  // 메시지 불러오기
-  const messages = await getMessages();
+    // 메시지 불러오기
+    const messages = await getMessages();
 
-  return (
-      <html lang={locale} className="dark" suppressHydrationWarning>
-      <body>
+    return (
+        <html lang={locale} className="dark" suppressHydrationWarning>
+        <body>
         <ThemeProvider>
-          <NextIntlClientProvider messages={messages}>
-            {children}
-            {/* 포탈 위치 지정 */}
-            <div id="portal-root" ></div>
-          </NextIntlClientProvider>
+            <NextIntlClientProvider messages={messages}>
+                {/*  여기서 children을 공통 래퍼로 감싸줌 (페이지 공통 래퍼)*/}
+                <div className="min-w-[375px] bg-background text-foreground">
+                    {children}
+                </div>
+
+                {/* 포탈 위치 지정 */}
+                <div id="portal-root" />
+            </NextIntlClientProvider>
         </ThemeProvider>
-      </body>
-      </html>
-  );
+        </body>
+        </html>
+    );
 }
