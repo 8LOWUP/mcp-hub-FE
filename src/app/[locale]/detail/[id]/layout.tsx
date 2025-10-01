d62ca5291f287b5c6a73ef63eb45661e0ba6e5f3
vicky <vicky0523@naver.com>
2025-10-01 19:11:00 +0900

    [feat]:sparkles: 상세페이지 라우팅 수정

---
+++ src/app/[locale]/detail/[id]/layout.tsx
@@ -0,0 +1,9 @@
+export default function DetailLayout({ children }: { children: React.ReactNode }) {
+    return (
+        <div className="flex flex-col min-h-screen">
+            <div className="flex-1 w-full max-w-7xl mx-auto px-4 md:px-8">
+                {children}
+            </div>
+        </div>
+    );
+}
