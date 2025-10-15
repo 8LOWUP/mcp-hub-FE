7983f1d714aec50eb2329fe35a1dd853666b6a75
smileman62 <jskim6335@naver.com>
2025-10-15 15:02:30 +0900

    찐 최종 커밋 #2

--- src/features/detail/components/McpDetails.tsx
+++ src/features/detail/components/McpDetails.tsx
@@ -3,10 +3,7 @@
 import TextContainer from "@/components/container/TextContainer";
 import Image from "next/image";
 import type { getMcpDetailResponse } from "@/types/detail/detail-types";
-<<<<<<< HEAD
 import imageLoader from "@/lib/imageLoader";
-=======
->>>>>>> e796a77 ([feat]:sparkles: 제목 컴포넌트 추가)
 import {BadgeAlert } from "lucide-react";
 
 interface Props {
